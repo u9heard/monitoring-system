f93c3857d6e3d0ed1569aa00beae9ed3afbacb99
Ilya <ilya.zotov.01@mail.ru>
2023-02-07 12:41:42 +0300

    Style edit

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField('Логин', validators=[DataRequired()], render_kw={"placeholder": "Логин"})
+    password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"placeholder": "Пароль"})
+    remember_me = BooleanField('Запомнить')
+    submit = SubmitField('Войти')
